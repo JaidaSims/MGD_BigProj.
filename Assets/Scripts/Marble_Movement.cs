74540fc28e8f13f8d97b6cc1f1718a7b701765bb
Student <student@DM009.local>
2019-10-31 09:28:07 -0500

    10-30, Tilt controls added

---
+++ Assets/Scripts/Marble_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Marble_Movement : MonoBehaviour
+{
+
+    Vector3 dir;
+    Rigidbody rb;
+
+    public bool debug = true;
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // dir = Vector3.zero; //New Vector3(0, 0, 0)
+
+        dir.x = Input.acceleration.x;
+        dir.z = Input.acceleration.y;
+
+        if(debug) {
+            Debug.DrawRay(this.transform.position, dir * 2, Color.black, 0.5f);
+        }
+    }
+
+    void FixedUpdate(){
+        rb.AddForce(dir * speed);
+    }
+}
